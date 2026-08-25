b7b3e856bf3c23d83f58e21be01cd0f70863c96d
asekovaadema-star <asekovaadema@gmail.com>
2026-08-25 15:53:43 +0600

    hw2

--- product/models.py
+++ product/models.py
@@ -10,11 +10,21 @@ class Product(models.Model):
     title = models.CharField(max_length=50)
     description = models.TextField(null=True, blank=True)
     price = models.FloatField()
-    category = models.ForeignKey(Category, on_delete=models.CASCADE)
+    category = models.ForeignKey(Category, on_delete=models.CASCADE, related_name='products')
 
     def __str__(self):
         return self.title
 
 class Review(models.Model):
     text = models.TextField(null=True, blank=True)
-    product = models.ForeignKey(Product, on_delete=models.CASCADE)
+    stars = models.IntegerField(choices=((i, i * '* ') for i in range(1, 6)),
+                                default=5)
+    product = models.ForeignKey(Product, on_delete=models.CASCADE, related_name='reviews')
+
+    # def rating(self, stars):
+    #     if not stars:
+    #         return 0.0
+    #     return round(sum(stars)/ len(stars), 1)
+
+    def __str__(self):
+        return self.text  
--- product/serializers.py
+++ product/serializers.py
@@ -1,23 +1,15 @@
 from rest_framework import serializers
 from .models import Category, Product, Review
 
-
-class ProductDetailListSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = Product  
-        fields = "__all__"
-
-
-class ProductListSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = Product 
-        fields = ("id", "price")  
-
-
 class CategoryListSerializer(serializers.ModelSerializer):
+    product_count = serializers.SerializerMethodField()
     class Meta:
         model = Category  
-        fields = "__all__"  
+        fields = ('id', 'name', 'product_count')
+
+    def get_product_count(self, obj):
+        return len([product for product in obj.products.all()])
+
 
 
 class CategoryDetailSerializer(serializers.ModelSerializer):
@@ -37,3 +29,24 @@ class ReviewDetailSerializer(serializers.ModelSerializer):
     class Meta:
         model = Review  
         fields = "__all__" 
+
+class ProductDetailListSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Product  
+        fields = "__all__"
+
+
+class ProductListSerializer(serializers.ModelSerializer):
+    reviews = ReviewDetailSerializer(many = True)
+    rating = serializers.SerializerMethodField()
+    class Meta:
+        model = Product 
+        fields = ("title",
+                  "id", 
+                  "price", 
+                  "reviews",
+                  'rating') 
+
+    def get_rating(self, obj):
+        stars = [review.stars for review in obj.reviews.all()]
+        return round(sum(stars)/ len(stars), 1 ) if stars else 0.0
